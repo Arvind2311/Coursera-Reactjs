42a180d6f9ff386d745ecef07ebaf5b111b98fab
Arvind Prasad <arvind231195@gmail.com>
2019-01-27 22:16:20 +0530

    Assignment 3

--- Week 3/confusion/src/components/DishdetailComponent.js
+++ Week 3/confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 
 function RenderDish({dish}){
     return(
@@ -31,6 +32,7 @@ function RenderComments({comments}){
             <ul className="list-unstyled">
                 {comment}
             </ul>
+            <CommentForm />
         </div>
     );
 };
