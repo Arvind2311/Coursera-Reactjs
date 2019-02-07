241eb2f80862d69c2e7272fa5e52813e82fee5d2
Arvind Prasad <arvind231195@gmail.com>
2019-02-07 19:10:00 +0530

    Fetch Post Comment

--- Week 4/confusion/src/components/DishdetailComponent.js
+++ Week 4/confusion/src/components/DishdetailComponent.js
@@ -23,7 +23,7 @@ function RenderDish({dish}){
     );
 };
 
-function RenderComments({comments, addComment, dishId}){
+function RenderComments({comments, postComment, dishId}){
     const comment = comments.map((comment) => {
         return (
             <span>
@@ -38,7 +38,7 @@ function RenderComments({comments, addComment, dishId}){
             <ul className="list-unstyled">
                 {comment}
             </ul>
-            <CommentForm dishId={dishId} addComment={addComment} />
+            <CommentForm dishId={dishId} postComment={postComment} />
         </div>
     );
 };
@@ -79,7 +79,7 @@ const DishDetail = (props) => {
             <div className="row">
                 <RenderDish dish={props.dish} />
                 <RenderComments comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     dishId={props.dish.id} />
             </div>
             </div>
@@ -109,7 +109,7 @@ class CommentForm extends Component{
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
